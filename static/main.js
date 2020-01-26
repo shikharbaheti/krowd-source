bcea42e5b02529966ac2df597b89574ca8bf1785
Shikhar Baheti <46034575+shikharbaheti@users.noreply.github.com>
2020-01-25 20:23:12 -0600

    updated

--- static/main.js
+++ static/main.js
@@ -1,3 +1,33 @@
-function sayHello() {
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    getLocation();
+
+});
+
+function myFunction() {
     alert("Hello World")
+}
+
+var x = document.getElementById("location");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+    console.log("This was called.")
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+    console.log("This was called too.");
+}
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 50.2, lng: 50.3 },
+        zoom: 8
+    });
 }
